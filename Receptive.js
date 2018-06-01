e927b1ddd4ab03097e128c4d63e3c52705606fe7
Takeru_Kodama <m14069@g.metro-cit.ac.jp>
2018-06-01 15:53:18 +0900

    some change

--- Receptive.js
+++ Receptive.js
@@ -1,10 +1,12 @@
+const h = 1 / 10;
+
 class Receptive {
 
   /**
   *
   * constructor
   *
-  * @param {boolean} mode true: ON, false: OFF
+  * @param {Boolean} mode true: ON, false: OFF
   * @param {Number} sells number of sells
   *
   */
@@ -114,6 +116,39 @@ class Receptive {
 
     return sum;
   }
+
+  /**
+  *
+  * emit: simuration emittion.
+  *
+  * @param {Number} duration
+  * @param {Number} noiseRange
+  *
+  * @return {Array}
+  *
+  */
+  emit(duration, noiseRange) {
+    let output = [];
+
+    for(let sell of this.sells) {
+      let v = [];
+      let w = [];
+
+      v.push(0);
+      w.push(0);
+
+      for(let i = 1; i < duration; i++) {
+        let res;
+
+        res = runge(v[i - 1], w[i - 1], sell.output(), noiseRange * noise(1));
+
+        v.push(res.mv);
+        w.push(res.mw);
+      }
+      output.push(v);
+    }
+    return output;
+  }
 }
 
 function dog(sige, sigi, x) {
@@ -121,4 +156,61 @@ function dog(sige, sigi, x) {
   let i = (1/Math.pow(sigi, 2)) * Math.exp(-1*Math.pow(x, 2)/(2*Math.exp(sigi, 2)));
 
   return e - i;
+}
+
+function runge(v, w, I, n) {
+  let kv = [];
+  let mv;
+  let kw = [];
+  let mw;
+
+  kv.push(h * mem(v, w, I, n));
+  kw.push(h * inact(v, w));
+
+  kv.push(h * mem(v + (1/2)*kv[0], w + (1/2)*kw[0], I, n));
+  kw.push(h * inact(v + (1/2)*kv[0], w + (1/2)*kw[0]));
+
+  kv.push(h * mem(v + (1/2)*kv[1], w + (1/2)*kw[1], I, n));
+  kw.push(h * inact(v + (1/2)*kv[1], w + (1/2)*kw[1]));
+
+  kv.push(h * mem(v + kv[2], w + kw[2], I, n));
+  kw.push(h * inact(v + kv[2], w + kw[2]));
+
+  mv = v + (kv[0] + 2*kv[1] + 2*kv[2] + kv[3]) / 6;
+  mw = w + (kw[0] + 2*kw[1] + 2*kw[2] + kw[3]) / 6;
+
+  return {mv, mw};
+}
+
+function mem(v, w, I, n) {
+  return c * (v - Math.pow(v, 3) / 3 - w + I + n);
+}
+
+function inact(v, w) {
+  return v + a - b * w;
+}
+
+/**
+*
+* count emittion
+*
+* @param {Array} vdata
+*
+* @return {Array}
+*
+*/
+function countEmittion(vdata) {
+  let res = [];
+
+  for(let data of vdata) {
+    let counter = 0;
+    for(let i = 0; i < data.length; i++) {
+      if(Math.sign(data[i]) != Math.sign(data[i+1])) {
+        counter++;
+      }
+    }
+    counter / 2;
+    res.push(counter);
+  }
+  return res;
 }
--- sketch.js
+++ sketch.js
@@ -1,8 +1,9 @@
 const a = 0.7;
 const b = 0.5;
 const c = 10;
-const h = 1 / 100;
 const r = new Receptive(true, 30);
+
+const isNoise = false;
 
 let I;
 let v = [];
@@ -20,63 +21,50 @@ function setup() {
 
   r.set(5);
   r.setWeight();
-  r.inputToSell(10, 12, 0);
-  r.inputToSell(13, 17, 0.1);
 
-  I = r.sells[11].output();
+  let count = 0
 
-  console.log(I)
+  for(let i = 0; i < r.sells.length; i+=6) {
+    let inputValue;
+
+    if(count % 2) inputValue = 0;
+    else inputValue = 0.05;
+
+    r.inputToSell(i, i+5, inputValue);
+
+    count++;
+  }
+
+  I = r.sells[13].output();
+
   console.log(r.isWhereMid())
 
   x = width / 2;
+
+  let output = r.emit(1000, 0.2);
+  let emittionCount = countEmittion(output);
+  console.log(emittionCount);
 }
 
 function draw() {
 
   // background(91);
 
-  let res = runge(v[frameCount - 1], w[frameCount - 1], I, noise(frameCount) / 5);
+  // let res;
+  //
+  // if(isNoise) res = runge(v[frameCount - 1], w[frameCount - 1], I, 0.3*noise(1));
+  // else res = runge(v[frameCount - 1], w[frameCount - 1], I, 0);
+  //
+  // v.push(res.mv);
+  // w.push(res.mw);
+  //
+  // x += 0.1;
+  //
+  // point(x, height - 200 + v[frameCount - 1] * 50);
+  // if(x > width) {
+  //   x = 0;
+  //   background(121);
+  // }
 
-  v.push(res.mv);
-  w.push(res.mw);
 
-  x += 0.1;
-
-  point(x, height - 200 + v[frameCount - 1] * 50);
-  if(x > width) {
-    x = 0;
-    background(121);
-  }
-}
-
-function runge(v, w, I, n) {
-  let kv = [];
-  let mv;
-  let kw = [];
-  let mw;
-
-  kv.push(h * mem(v, w, I, n));
-  kw.push(h * inact(v, w));
-
-  kv.push(h * mem(v + (1/2)*kv[0], w + (1/2)*kw[0], I, n));
-  kw.push(h * inact(v + (1/2)*kv[0], w + (1/2)*kw[0]));
-
-  kv.push(h * mem(v + (1/2)*kv[1], w + (1/2)*kw[1], I, n));
-  kw.push(h * inact(v + (1/2)*kv[1], w + (1/2)*kw[1]));
-
-  kv.push(h * mem(v + kv[2], w + kw[2], I, n));
-  kw.push(h * inact(v + kv[2], w + kw[2]));
-
-  mv = v + (kv[0] + 2*kv[1] + 2*kv[2] + kv[3]) / 6;
-  mw = w + (kw[0] + 2*kw[1] + 2*kw[2] + kw[3]) / 6;
-
-  return {mv, mw};
-}
-
-function mem(v, w, I, n) {
-  return c * (v - Math.pow(v, 3) / 3 - w + I + n);
-}
-
-function inact(v, w) {
-  return v + a - b * w;
 }
